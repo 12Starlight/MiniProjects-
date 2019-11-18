6904ecc5fad93ca21f0e37a32baf0f23cf1d7cf1
Dave <dave.gagnat@gmail.com>
2019-11-17 20:52:54 -0600

    Created allTodos selector and put it on the window to test, drilled 3 times

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { receiveTodos, receiveTodo } from './actions/todo_actions';
 import Root from './components/root';
+import { allTodos } from './reducers/selectors';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,7 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
   window.store = store; 
   window.receiveTodos = receiveTodos;
   window.receiveTodo = receiveTodo; 
-
+  window.allTodos = allTodos; 
+  
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root); 
 });
+
